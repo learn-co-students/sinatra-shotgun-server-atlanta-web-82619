e79da5737157831f81b244c4b3783896f6998ad8
Raza <rtshareef@gmail.com>
2019-09-15 01:48:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! SHOTGUN GEM ALLOWS ME TO EDIT WITHOUT RESTARTING THE APP.rb. "
   end
 
 end
